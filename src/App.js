9a7c2c4bb09284e195f5e6fdb397f95554507a81
Nick Tillinghast <nickt2@cox.net>
2020-10-23 23:24:43 -0600

    33

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-
-      ]
+      monsters: [],
+      searchField: ''
     }
 
   }
@@ -21,13 +20,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Nick" >
-          {
-            this.state.monsters.map(monster => (
-              <h1 key={monster.id}>{monster.name}</h1>
-            ))
-          }
-        </CardList>
+        <input type='search' placeholder='search monsters' onChange={e =>
+          this.setState({ searchField: e.target.value }
+          )}
+
+        />
+        <CardList monsters={this.state.monsters} />
+
+
 
       </div>
     );
