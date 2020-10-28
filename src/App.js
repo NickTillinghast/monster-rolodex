e8880a71cb1622c98cbe6abb71606c534d91d718
Nick Tillinghast <nickt2@cox.net>
2020-10-27 22:57:37 -0600

    close to done

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         {/* <input
           type='search'
           placeholder='search monsters'
